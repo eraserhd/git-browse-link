9fa8d46d015f4703e90f0b6edfca639e7d83acea
Jason Felice <jason.m.felice@gmail.com>
2020-03-11 14:36:10 -0400

    Add run-tests.elv

--- release.nix
+++ release.nix
@@ -3,8 +3,16 @@ let
   pkgs = import nixpkgs { config = {}; };
   git-browse-link = pkgs.callPackage ./derivation.nix {};
 in {
-  test = pkgs.runCommandNoCC "git-browse-link-test" {} ''
-    mkdir -p $out
-    : ${git-browse-link}
-  '';
+  test = pkgs.stdenv.mkDerivation {
+    name = "tests";
+    src = ./.;
+    buildInputs = [ pkgs.elvish ];
+
+    buildPhase = ''
+      ${pkgs.elvish}/bin/elvish run-tests.elv
+    '';
+    installPhase = ''
+      touch $out
+    '';
+  };
 }

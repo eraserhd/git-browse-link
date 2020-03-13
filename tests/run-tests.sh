51aaea343b86bb9ba95bab52da5e2e175a9c9594
Jason Felice <jason.m.felice@gmail.com>
2020-03-12 22:38:48 -0400

    Handle github https:// links

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -77,4 +77,9 @@ t 'translates git@github.com origins into github browse links' \
     -command 'git browse-link repo/dir/file.txt' \
     -output 'https://github.com/foo/bar/blob/ce1ced46695def162cadf35f7f02df67cd215c60/dir/file.txt'
 
+t 'translates https://github.com origins into github browse links' \
+    -remote origin https://github.com/foo/bar.git \
+    -command 'git browse-link repo/dir/file.txt' \
+    -output 'https://github.com/foo/bar/blob/ce1ced46695def162cadf35f7f02df67cd215c60/dir/file.txt'
+
 summarize

bef7b68221bb45b53741a4b5d89fb91cd4c553ee
Jason Felice <jason.m.felice@gmail.com>
2020-03-12 22:52:53 -0400

    Test for git@github.com origin without .git suffix

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -12,6 +12,11 @@ t 'translates https://github.com origins into github browse links' \
     -command 'git browse-link repo/dir/file.txt' \
     -output 'https://github.com/foo/bar/blob/ce1ced46695def162cadf35f7f02df67cd215c60/dir/file.txt'
 
+t 'translates git@github.com origins without .git suffix into github browse links' \
+    -remote origin git@github.com:foo/bar \
+    -command 'git browse-link repo/dir/file.txt' \
+    -output 'https://github.com/foo/bar/blob/ce1ced46695def162cadf35f7f02df67cd215c60/dir/file.txt'
+
 t 'translates https://github.com origins without .git suffix into github browse links' \
     -remote origin https://github.com/foo/bar \
     -command 'git browse-link repo/dir/file.txt' \

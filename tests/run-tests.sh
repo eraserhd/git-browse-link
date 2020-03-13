fd506b210be7b18065959ec2222bd08c28b1d7a5
Jason Felice <jason.m.felice@gmail.com>
2020-03-12 22:58:08 -0400

    Add tests for github anchors

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -22,4 +22,16 @@ t 'translates https://github.com origins without .git suffix into github browse 
     -command 'git browse-link repo/dir/file.txt' \
     -output 'https://github.com/foo/bar/blob/ce1ced46695def162cadf35f7f02df67cd215c60/dir/file.txt'
 
+## Positions
+
+t 'adds line anchor for a single line when position is on a single line' \
+    -remote origin https://github.com/foo/bar \
+    -command 'git browse-link repo/dir/file.txt 3.4,3.6' \
+    -output 'https://github.com/foo/bar/blob/ce1ced46695def162cadf35f7f02df67cd215c60/dir/file.txt#L3'
+
+t 'adds line anchor for multiple lines when position spans multiple lines' \
+    -remote origin https://github.com/foo/bar \
+    -command 'git browse-link repo/dir/file.txt 3.4,7.6' \
+    -output 'https://github.com/foo/bar/blob/ce1ced46695def162cadf35f7f02df67cd215c60/dir/file.txt#L3-L7'
+
 summarize
